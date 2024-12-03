b8bbb93ea062cba46263577e3ef54e0cddb9ef88
wen <wenliang1.meng@cotticoffee.com>
2024-12-03 19:22:43 +0800

    add admin

--- app/types/navigation.ts
+++ app/types/navigation.ts
@@ -1,41 +1,41 @@
-export interface NavigationSubItem {
-    title: string
-    titleEn: string
-    description: string
-    descriptionEn: string
-    icon: string
-    href: string
-  }
-  
-  export interface NavigationSubCategory {
-    id: string
-    title: string
-    items: NavigationSubItem[]
-  }
-  
-  export interface NavigationItem {
+export interface NavigationItem {
     id: string
     title: string
     icon: string
-    items: NavigationSubItem[]
-    subCategories: NavigationSubCategory[]
-  }
+    items: {
+        title: string
+        href: string
+    }[]
+    subCategories: {
+        id: string
+        title: string
+        items: {
+            title: string
+            href: string
+        }[]
+    }[]
+}
   
-  export interface NavigationData {
+export interface NavigationData {
     navigationItems: NavigationItem[]
-  }
+}
   
-  export interface ResourceItem {
+export interface ResourceItem {
     title: string
     description: string
     icon: string
     url: string
-  }
+}
   
-  export interface ResourceSection {
+export interface ResourceSection {
     id: string
     title: string
     items: ResourceItem[]
+}
+  
+export interface ResourceData {
+    resourceSections: ResourceSection[]
+}
   }
   
   export interface ResourceData {

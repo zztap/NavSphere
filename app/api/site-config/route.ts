c3f052b6c92709016ee64f5238e1711c090280fa
wen <wenliang1.meng@cotticoffee.com>
2024-12-09 03:57:11 +0800

    优化后台框架

--- app/api/site-config/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export const runtime = 'edge'
-
-export async function GET() {
-  return NextResponse.json({ 
-    message: 'Site configuration endpoint' 
-  })
-}

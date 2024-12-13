206a9bb8684595ef926986c6b8119fc50a4df5aa
wen <wenliang1.meng@cotticoffee.com>
2024-12-13 23:22:41 +0800

    优化上传

--- app/admin/resources/page.tsx
+++ app/admin/resources/page.tsx
@@ -97,7 +97,6 @@ export default function ResourceManagement() {
     resolver: zodResolver(formSchema),
     defaultValues: {
       resource: {
-        path: "",
         image: undefined,
       },
     },
